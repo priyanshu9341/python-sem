5f12916be4a2867106558143b8ef983923916dca
priyanshu9341 <priyanshukumar79037@gmail.com>
2025-01-19 20:42:21 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+import speech_recognition as sr
+import pyttsx3
+# initialize the recognizer
+r = sr.Recognizer()
+
+def record_text():
+    #loop in case of errors
+    while(1):
+        try:
+            #use the microphone as source for input.
+            with sr.Microphone() as source2:
+                 #prepare recognizer to receive input
+                r.adjust_for_ambient_noise(source2,duration=0.2)
+                
+                #listens for the user's input
+                audio2 = r.listen(source2)
+                
+                # using google to recognize audio
+                MyText = r.recognize_google(audio2)  # recognize_audio provide us an Api which converts "voice command" to "text".
+                
+                return MyText
+                
+        except sr.RequestError as e:
+            print("Could not request results; {0}".format(e))
+                    
+        except sr.UnknownValueError:
+            print("unknown error occurred")   
+                 
+    return
+
+def output_text(text):
+    f=open("output.txt","a")
+    f.write(text)
+    f.write("\n")
+    f.close()
+    return
+
+while(1):
+    text = record_text()
+    output_text(text)
+    
+    print("Wrote text")
